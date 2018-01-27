20d099d6616af89d96263a05399a88ccac95927c
Veronica Kim <vero.a.kim@gmail.com>
2018-01-27 12:41:02 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,10 +3,13 @@ require 'pry'
 def my_find(collection)
   i = 0
   new_block = []
-  
+
   while i < collection.length
   new_block << yield
   (collection[i])
   i += 1
   end
 end
+
+collection = (1..100).to_a
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
