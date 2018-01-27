25e98ba32d227fc00733eb3ac59eace614451302
Veronica Kim <vero.a.kim@gmail.com>
2018-01-27 12:40:12 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,8 +2,11 @@ require 'pry'
 
 def my_find(collection)
   i = 0
+  new_block = []
+  
   while i < collection.length
-  collection << yield
+  new_block << yield
+  (collection[i])
   i += 1
   end
 end
